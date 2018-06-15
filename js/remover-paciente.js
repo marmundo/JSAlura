2cd6d3a2eac5e1aa0ed20d2e59203a7ee5c0eb16
Marcelo Damasceno <marcelodamasceno@gmail.com>
2018-06-15 16:06:40 -0300

    refatoração da logica do botão remover paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,10 +3,8 @@
 function adicionaComportamentoBotaoRemover(){
 	var botoesRemover = document.querySelectorAll(".fa-times");
 	botoesRemover.forEach(function(botaoRemover) {
-			botaoRemover.addEventListener("click", function(event) {
-	    		event.preventDefault();    
-	     		var row = botaoRemover.parentNode.parentNode;    		
-	    		row.parentNode.removeChild(row);
+			botaoRemover.addEventListener("click", function(event) {	    		
+	     		botaoRemover.parentNode.parentNode.remove(); 			    		
 			});
 		});
 }
